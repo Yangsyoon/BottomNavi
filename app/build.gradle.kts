8048ce216d624c2ac7afc44e004551dc1bd703c1
Yooseunghyeok <yuyuyu0326@naver.com>
2024-06-23 15:56:30 +0900

    버튼

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,7 @@ dependencies {
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.google.android.material:material:1.4.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
-
+    implementation ("com.google.android.material:material:1.3.0")
 
     //swipe layout
 //    implementation("com.chauthai.swipereveallayout:swipe-reveal-layout:1.4.1")
